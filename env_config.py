e0d9b1066658518388cb40397f4cd8b832b762ed
kalpesh33in-max <kalpesh33in@gmail.com>
2026-03-11 06:25:02 +0530

    Create env_config.py

---
+++ env_config.py
@@ -0,0 +1,9 @@
+import os
+
+# Zerodha Credentials
+API_KEY = os.getenv("API_KEY", "YOUR_API_KEY")
+API_SECRET = os.getenv("API_SECRET", "YOUR_API_SECRET")
+
+# Telegram Credentials
+TELE_TOKEN = os.getenv("TELE_TOKEN", "8587757379:AAEa1zQmNAN8xcYLaQlXzsMzqph3bNqUpgg")
+TELE_CHAT_ID = os.getenv("TELE_CHAT_ID", "YOUR_CHAT_ID")
