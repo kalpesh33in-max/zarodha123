f7fe14f0eb1c3169f8109ef2498244f5d4e30499
kalpesh33in-max <kalpesh33in@gmail.com>
2026-03-12 20:46:31 +0530

    Update telegram_utils.py

--- telegram_utils.py
+++ telegram_utils.py
@@ -1,16 +1,21 @@
 import requests
 from env_config import TELE_TOKEN, TELE_CHAT_ID
 
-def send_telegram_message(message):
+def send_telegram_message(message, chat_id=None, token=None):
 
-    if not TELE_TOKEN or not TELE_CHAT_ID:
-
-        print("Telegram configuration missing!")
+    target_token = token if token else TELE_TOKEN
+    if not target_token:
+        print("Telegram token missing!")
         return
 
-    url = f"https://api.telegram.org/bot{TELE_TOKEN}/sendMessage"
+    target_id = chat_id if chat_id else TELE_CHAT_ID
+    if not target_id or target_id == "YOUR_CHAT_ID":
+        print(f"Target Chat ID missing: {target_id}")
+        return
+
+    url = f"https://api.telegram.org/bot{target_token}/sendMessage"
     payload = {
-        "chat_id": TELE_CHAT_ID,
+        "chat_id": target_id,
         "text": message
     }
 
