42594c9550d066a0456b3d3c7566081ada5abbfa
kalpesh33in-max <kalpesh33in@gmail.com>
2026-03-17 13:45:34 +0530

    Update env_config.py

--- env_config.py
+++ env_config.py
@@ -1,22 +1,23 @@
 import os
 
-# Zerodha
+# ================= ZERODHA =================
 API_KEY = os.getenv("KITE_API_KEY")
 API_SECRET = os.getenv("KITE_API_SECRET")
 
-# Login
 USER_ID = os.getenv("KITE_USER_ID")
 PASSWORD = os.getenv("KITE_PASSWORD")
 TOTP_SECRET = os.getenv("KITE_TOTP_SECRET")
 
-# Telegram (ONLY VARIABLES)
+# ================= TELEGRAM TOKENS =================
 TELE_TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 TELE_TOKEN_BN = os.getenv("TELE_TOKEN_BN")
 TELE_TOKEN_STOCKS = os.getenv("TELE_TOKEN_STOCKS")
 TELE_TOKEN_VELOCITY = os.getenv("TELE_TOKEN_VELOCITY")
 
+# ================= TELEGRAM CHAT IDS =================
 TELE_CHAT_ID = os.getenv("CHAT_ID")
+
 TELE_CHAT_ID_BN = os.getenv("CHAT_ID_BN")
 TELE_CHAT_ID_STOCKS = os.getenv("CHAT_ID_STOCKS")
 TELE_CHAT_ID_VELOCITY = os.getenv("CHAT_ID_VELOCITY")
