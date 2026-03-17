87b14b039416c19f48eae957bf92a6164b748bac
kalpesh33in-max <kalpesh33in@gmail.com>
2026-03-17 13:10:52 +0530

    Update telegram_utils.py

--- telegram_utils.py
+++ telegram_utils.py
@@ -1,5 +1,5 @@
 import requests
-from env_config import TELE_TOKEN, TELE_CHAT_ID
+from env_config import TELE_TOKEN
 
 def send_telegram_message(message, chat_id=None, token=None):
 
@@ -8,14 +8,14 @@ def send_telegram_message(message, chat_id=None, token=None):
         print("Telegram token missing!")
         return
 
-    target_id = chat_id if chat_id else TELE_CHAT_ID
-    if not target_id or target_id == "YOUR_CHAT_ID":
-        print(f"Target Chat ID missing: {target_id}")
+    if not chat_id:
+        print("Chat ID missing!")
         return
 
     url = f"https://api.telegram.org/bot{target_token}/sendMessage"
+
     payload = {
-        "chat_id": target_id,
+        "chat_id": chat_id,
         "text": message
     }
 
@@ -23,5 +23,5 @@ def send_telegram_message(message, chat_id=None, token=None):
         response = requests.post(url, json=payload)
         return response.json()
     except Exception as e:
-        print(f"Error sending Telegram message: {e}")
+        print(f"Telegram Error: {e}")
         return None
